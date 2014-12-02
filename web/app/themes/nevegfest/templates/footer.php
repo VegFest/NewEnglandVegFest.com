733171bd6f7789444528f7773507a5dd2cd3373e
Greg Opperman <gopperman@gmail.com>
2014-12-02 17:33:49 -0500

    Footer and Signup

--- web/app/themes/nevegfest/templates/footer.php
+++ web/app/themes/nevegfest/templates/footer.php
@@ -2,6 +2,49 @@
   <div class="container-fluid">
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
+	<div id="footer-info" class="bluebg">
+		<div class="container-fluid">
+			<div class="col-sm-4">
+				<img src="/app/themes/nevegfest/assets/img/carrot-footer.png" alt="" class="alignright" />
+				<div class="pad">
+					<h3>Location</h3>
+					<p>DCU CENTER<br />
+						50 Foster St<br />
+						Worcester, MA 01608
+					</p>
+					<p><a href="https://www.google.com/maps/place/DCU+Center/@42.265458,-71.79876,15z/data=!4m2!3m1!1s0x0:0xc9f01cb8c1571d7a" target="_blank" rel="nofollow">Directions</a> | 
+						<a href="https://www.google.com/maps/search/parking+near+dcu+center/@42.2655405,-71.7987995,17z/data=!3m1!4b1?sa=X&ei=YS1-VP6cJYqpNojkgLgG&ved=0CJsBEMgT" target="_blank" rel="nofollow">Parking</a>
+					</p>
+				</div>
+			</div>
+			<div class="col-sm-8" class="pad">
+				<div class="pad">
+					<h3>Signup for VegFest News</h3>
+					<!-- Begin MailChimp Signup Form -->
+					<div id="mc_embed_signup">
+					<form action="//VegWorcester.us2.list-manage.com/subscribe/post?u=38963ea1aa7f978ae2c06864a&amp;id=42306f7ec2" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	
+<div class="mc-field-group">
+	<label for="mce-EMAIL" class="hidden">Email Address </label>
+	<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="you@email.com">
+	<input type="hidden" value="VegFest 2015 Website" name="SOURCE" id="mce-SOURCE">
+</div>
+	<div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;"><input type="text" name="b_38963ea1aa7f978ae2c06864a_42306f7ec2" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="GO" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
+				</div>
+			</div>
+		</div>
+	</div>
 	<div id="credit" class="container-fluid">
 		<a href="http://vegworcester.com" target="_blank"><img src="/app/themes/nevegfest/assets/img/VegWorcester.png" alt="Presented by VegWorcester" /></a>
 	</div>
