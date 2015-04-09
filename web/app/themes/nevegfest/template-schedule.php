df8cabe773060f61f3204a0b129017a82caa8572
Greg Opperman <gopperman@gmail.com>
2015-04-09 00:19:02 -0400

    Italics errywhere

--- web/app/themes/nevegfest/template-schedule.php
+++ web/app/themes/nevegfest/template-schedule.php
@@ -16,8 +16,8 @@ Template Name: Schedule Template
 				<img src="/app/themes/nevegfest/assets/img/nickcooney.jpg" alt="Nick Cooney" />
 			</div>	
 			<div class="col-sm-9">
-				<h2>Nick Cooney, <i>Director of Education Mercy For Animals</i></h2>
-				<p>Nick Cooney is the author of Change Of Heart: What Psychology Can Teach Us About Spreading Social Change and Veganomics: The Surprising Science on What Motivates Vegetarians, from the Breakfast Table to the Bedroom. He is Director of Education at Mercy For Animals, a national non-profit animal protection organization, and founder of The Humane League. His work has been featured in hundreds of media outlets, including the Wall Street Journal, TIME magazine, and National Public Radio, and he has lectured extensively throughout the United States and Europe on how to carry out charity effectively.</p>
+				<h2>Nick Cooney <i>Director of Education, Mercy For Animals</i></h2>
+				<p>Nick Cooney is the author of <i>Change Of Heart: What Psychology Can Teach Us About Spreading Social Change</i> and <i>Veganomics: The Surprising Science on What Motivates Vegetarians, from the Breakfast Table to the Bedroom</i>. He is Director of Education at Mercy For Animals, a national non-profit animal protection organization, and founder of The Humane League. His work has been featured in hundreds of media outlets, including the Wall Street Journal, TIME magazine, and National Public Radio, and he has lectured extensively throughout the United States and Europe on how to carry out charity effectively.</p>
 			</div>
 		</div>
 	</div>
@@ -40,7 +40,7 @@ Template Name: Schedule Template
 			</div>	
 			<div class="col-sm-9">
 				<h2>Lindsay Nixon <i>Author, Happy Herbivore</i></h2>
-				<p>Lindsay S. Nixon is the bestselling author of the Happy Herbivore Cookbook Series. Since 2011, Nixon has sold more than 200,000 books. Her work has been featured on The Food Network, Dr. Oz, in the New York Times, Vegetarian Times magazine, Shape Magazine, Fitness Magazine, Women's Health and WebMD, among others. A rising star in the culinary world, Nixon is praised for her ability to use everyday ingredients to create healthy, low fat recipes that make eating well easy, affordable, realistic and delicious. Learn more about Nixon and try some of her recipes on her award-winning blog <a href="http://happyherbivore.com/" target="_blank">happyherbivore.com</a></p>
+				<p>Lindsay S. Nixon is the bestselling author of the <i>Happy Herbivore</i> Cookbook Series. Since 2011, Nixon has sold more than 250,000 books. Her work has been featured on The Food Network, Dr. Oz, in the New York Times, Vegetarian Times magazine, Shape Magazine, Fitness Magazine, Women's Health and WebMD, among others. A rising star in the culinary world, Nixon is praised for her ability to use everyday ingredients to create healthy, low fat recipes that make eating well easy, affordable, realistic and delicious. Learn more about Nixon and try some of her recipes on her award-winning blog <a href="http://happyherbivore.com/" target="_blank">happyherbivore.com</a></p>
 			</div>
 		</div>
 	</div>
