bb905f625ec28d090bf3619a665cb5be44b2c6eb
Greg Opperman <gopperman@gmail.com>
2015-04-09 21:49:45 -0400

    Schedule ready to go

--- web/app/themes/nevegfest/template-schedule.php
+++ web/app/themes/nevegfest/template-schedule.php
@@ -65,6 +65,8 @@ Template Name: Schedule Template
 			</div>	
 			<div class="col-sm-9">
 				<h2>Flowforms Yoga</h2>
+				<p>Enjoy a family-friendly yoga session guided by local teachers from Worcester's Flowforms Yoga. Yoga makes your mind calm and peaceful and opens your heart. It tones, strengthens and heals your body. Presentation is appropriate for all ages, fitness levels and body types. *Bring your own yoga mat! </p>
+				<p><a href="//FlowformsYoga.com" target="_blank">Flowforms Yoga</a></p>
 			</div>
 		</div>
 	</div>
@@ -75,6 +77,8 @@ Template Name: Schedule Template
 			</div>	
 			<div class="col-sm-9">
 				<h2>Children's Book Reading</h2>
+				<p>Vegan Publishers do a live reading of <i>Dave Loves Chickens</i>. Do you love chickens? Dave does! Dave is a lovely monster from far far away that doesn’t understand why humans eat animals. Come with Dave and explore the fascinating world of chickens and their many talents. Written and illustrated by Carlos Patino. 
+				<p><a href="//veganpublishers.com/multimedia-archive/dave-loves-chickens/" target="_blank">Vegan Publishers: Dave Loves Chickens</a></p>
 			</div>
 		</div>
 	</div>
@@ -85,7 +89,8 @@ Template Name: Schedule Template
 			</div>	
 			<div class="col-sm-9">
 				<h2>Vegetable Circus</h2>
-				<p>Vegetable Circus uses circus arts to teach children and adults creative and exciting ways to live healthy. Their goal is to introduce children and adults to healthy, confidence-raising activities that support their development.</p>
+				<p>Vegetable Circus instructors teach the basics of a variety of circus arts to deliver an interactive experience. Everyone participates in circus themed group games and receives hands-on instruction in many circus skills. Fun for kids!</p>
+				<p><a href="//vegetablecircus.com" target="_blank">Vegetable Circus</a></p>
 			</div>
 		</div>
 	</div>
@@ -96,6 +101,8 @@ Template Name: Schedule Template
 			</div>	
 			<div class="col-sm-9">
 				<h2>Children's Book Reading</h2>
+				<p>Lena of Vegitopia and the Mystery of the Missing Animals is a vegan-themed fairy tale about how one brave little girl stands up for the animal friends of her land and helps rescue them from being eaten. The book promotes messages of kindness, compassion, and action and shows that magical things can happen when you harness the power of veggies.</p>
+				<p><a href="//veganpublishers.com/multimedia-archive/lena-of-vegitopia-and-the-mystery-of-the-missing-animals/" target="_blank">Vegan Publishers: Lena of Vegitopia</a></p>
 			</div>
 		</div>
 	</div>
