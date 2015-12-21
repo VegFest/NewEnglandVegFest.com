e3c0b0306aad3b2718649b5279de2c2bdb33752f
drewrwilson <drew.r.wilson@gmail.com>
2015-12-21 15:59:41 -0500

    update link to go directly to signup form

--- web/app/themes/nevegfest/templates/page-header.php
+++ web/app/themes/nevegfest/templates/page-header.php
@@ -5,7 +5,7 @@
 		<?php if (is_front_page()) { ?>
 			<div class="ctas">
 				<!-- <a class="cta" href="//www.facebook.com/events/355366494646694/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP</a> -->
-				<a class="cta" href="/sponsor-exhibitor-guide/">Sponsor & Exhibit Sign Up</a>
+				<a class="cta" href="/sponsor-and-exhibitor-application/">Sponsor & Exhibit Sign Up</a>
 			</div>
 			<h2>May 1, 2016 &middot; 11AM-5PM &middot; Worcester, MA</h2>
 		<?php } else { ?>
