3a3c0c44948b4a25cf91c5aac5f8c11a179e326b
drewrwilson <drew.r.wilson@gmail.com>
2015-12-21 15:31:25 -0500

    add event date and remove old schedule link from menu

--- web/app/themes/nevegfest/templates/page-header.php
+++ web/app/themes/nevegfest/templates/page-header.php
@@ -3,11 +3,11 @@
 	  <a href="/"><img itemprop="logo" src="/app/themes/nevegfest/assets/img/logo-white.png" alt="New England VegFest" /></a>
   </h1>
 		<?php if (is_front_page()) { ?>
-			<!--<div class="ctas">
-				<a class="cta" href="//www.facebook.com/events/355366494646694/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP</a>
-				<a class="cta" href="/sponsor-exhibitor-guide/">Sponsor / Exhibit</a>
-			</div>-->
-			<h2>Spring 2016 &middot; 11AM-5PM &middot; Worcester, MA</h2>
+			<div class="ctas">
+				<!-- <a class="cta" href="//www.facebook.com/events/355366494646694/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP</a> -->
+				<a class="cta" href="/sponsor-exhibitor-guide/">Sponsor & Exhibit Sign Up</a>
+			</div>
+			<h2>May 1, 2016 &middot; 11AM-5PM &middot; Worcester, MA</h2>
 		<?php } else { ?>
 			<h2><?= the_title(); ?></h2>
 		<?php } ?>
