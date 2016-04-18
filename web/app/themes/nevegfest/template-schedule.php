ae538ccb213e5c958a70eb31d9870882848fc724
drewrwilson <drew.r.wilson@gmail.com>
2016-04-18 14:18:07 -0400

    speaker sched

--- web/app/themes/nevegfest/template-schedule.php
+++ web/app/themes/nevegfest/template-schedule.php
@@ -13,11 +13,11 @@ Template Name: Schedule Template
 		<div class="container-fluid">
 			<div class="col-sm-3">
 				<h2>12PM</h2>
-				<img src="/app/themes/nevegfest/assets/img/nickcooney.jpg" alt="Nick Cooney" />
-			</div>	
+				<img src="/app/themes/nevegfest/assets/img/speakers/stephanie-bogdanich.jpg" alt="Taco Cleanse" />
+			</div>
 			<div class="col-sm-9">
-				<h2>Nick Cooney <i>Director of Education, Mercy For Animals</i></h2>
-				<p>Nick Cooney is the author of <i>Change Of Heart: What Psychology Can Teach Us About Spreading Social Change</i> and <i>Veganomics: The Surprising Science on What Motivates Vegetarians, from the Breakfast Table to the Bedroom</i>. He is Director of Education at Mercy For Animals, a national non-profit animal protection organization, and founder of The Humane League. His work has been featured in hundreds of media outlets, including the Wall Street Journal, TIME magazine, and National Public Radio, and he has lectured extensively throughout the United States and Europe on how to carry out charity effectively.</p>
+				<h2>Jessica Morris & Stephanie Bogdanich <i>authors The Taco Cleanse: The Tortilla Based Diet Proven To Change Your Life</i></h2>
+				<p>In 2013 Jessica and Stephanie, along with their fellow taco scientists in Austin Texas, embarked on a journey that would change the world forever. For 30 days they ate nothing but tacos for breakfast, lunch, dinner, and dessert. After years of rigorous testing they came up with their breakthrough cleanse plan <i>"The Taco Cleanse"</i>. It's the first ever diet plan the focuses on creating a happier life that promises to detox your dreams and redefine your dinner.</p>
 			</div>
 		</div>
 	</div>
@@ -25,40 +25,44 @@ Template Name: Schedule Template
 		<div class="container-fluid">
 			<div class="col-sm-3">
 				<h2>1PM</h2>
-				<img src="/app/themes/nevegfest/assets/img/plantbuilt.jpg" alt="Giacomo and Dani" />
-			</div>	
+				<img src="/app/themes/nevegfest/assets/img/speakers/beverly-lynn-bennett.jpg" alt="Beverly Lynn Bennett" />
+			</div>
 			<div class="col-sm-9">
-				<h2>Giacamo Marchese and Dani Taylor, PlantBuilt </h2>
-				<p>PlantBuilt is a 501(c)(3) non profit organization and a collaboration of vegan strength based athletes who compete annually to dispel the myths that vegans are weak and scrawny.  PlantBuilt also raises much needed funds for farm sanctuaries and vegan outreach.  Meet several team members from this years squad of bodybuilders, cross fitters, powerlifters, and kettle bell sport athletes, and find out why they believe that a message of compassion in athletics and competition is so important.  
+				<h2>Beverly Lynn Bennett <i>vegan chef, writer, animal advocate</i></h2>
+				<p>
+					<strong>"Spiralizing: The Fun and Easy Way to Turn Produce into Pasta”</strong>: Beverly Lynn Bennett with prepare Tzatziki-Style Cucumbers and Raw Pad Thai recipes
+				</p>
+				<p>Beverly Lynn Bennett is an experienced vegan chef and baker, writer, and animal advocate who is passionate about showing the world how easy, delicious, and healthful it is to live and eat as a vegan. A certified food-service operations manager, she earned her culinary arts degree in 1988 and gained much practical experience in the years that followed, working in and managing vegan and vegetarian restaurants and natural foods stores. Vegan since the early 1990s, Beverly is the author of Vegan Bites: Recipes for Singles, Chia: Using the Ancient Superfood, Kale: The Nutritional Powerhouse, Spiralize!: Transform Vegetables and Fruits from Ordinary to Extraordinary, The Pure Power of Maca, The Complete Idiot's Guide to Vegan Cooking, The Complete Idiot's Guide to Gluten-Free Vegan Cooking, and others. Her work has appeared in many national and international print publications, on public television and DVD, and all over the web. She has hosted The Vegan Chef website at <a href="http://www.veganchef.com">veganchef.com</a> since 1999, and lives in the very vegan-friendly city of Eugene, Oregon.
 </p>
 			</div>
 		</div>
 	</div>
-	<div class="presentation red">
+	<!-- <div class="presentation red">
 		<div class="container-fluid">
 			<div class="col-sm-3">
 				<h2>2PM</h2>
 				<img src="/app/themes/nevegfest/assets/img/lsn-photo.jpg" alt="Lindsay S. Nixon" />
-			</div>	
+			</div>
 			<div class="col-sm-9">
-				<h2>Lindsay Nixon <i>Author, Happy Herbivore</i></h2>
-				<p>Lindsay S. Nixon is the bestselling author of the <i>Happy Herbivore</i> Cookbook Series. Since 2011, Nixon has sold more than 250,000 books. Her work has been featured on The Food Network, Dr. Oz, in the New York Times, Vegetarian Times magazine, Shape Magazine, Fitness Magazine, Women's Health and WebMD, among others. A rising star in the culinary world, Nixon is praised for her ability to use everyday ingredients to create healthy, low fat recipes that make eating well easy, affordable, realistic and delicious. Learn more about Nixon and try some of her recipes on her award-winning blog <a href="http://happyherbivore.com/" target="_blank">happyherbivore.com</a></p>
+				<h2>Lorelei Plotczyk, <i>Truth or Drought</i></h2>
+				<p>Lorelei Plotczyk is an environmentalist and writer, who holds her MBA with an Environmental Management specialization. She's an organizer of Truth or Drought, a campaign to draw a connection between water scarcity and animal agriculture. Come to this presentation to learn about the ecological impacts of factory farming. She blogs at <a href="http://http://www.brainonhugs.com/">BrainOnHugs.com</a>.</p>
 			</div>
 		</div>
-	</div>
+	</div> -->
 	<div class="presentation yellow">
 		<div class="container-fluid">
 			<div class="col-sm-3">
 				<h2>3:30PM</h2>
 				<img src="/app/themes/nevegfest/assets/img/cowspiracy.png" alt="Cowspiracy" />
-			</div>	
+			</div>
 			<div class="col-sm-9">
-				<h2>Movie Screening: Cowspiracy</h2>
-				<p>Cowspiracy: The Sustainability Secret is a groundbreaking feature-length environmental documentary following intrepid filmmaker Kip Andersen as he uncovers the most destructive industry facing the planet today – Animal agriculture - and investigates why the world’s leading environmental organizations are too afraid to talk about it.</p>
+				<h2>Emily Moran Barwick <i>artist, activist, educator, "Bite Size Vegan"</i></h2>
+				<p>Emily created Bite Size Vegan, a popular collection of free, educational videos about veganism. Her mission is to simplify veganism and help people understand that it’s not an exclusive club, fad diet but rather a logical, compassionate, rational approach to life- especially in regards to the lives of others.</p>
 				<p><a href="http://vegfund.org" target="_blank">Sponsored by VegFund</a>
 			</div>
 		</div>
 	</div>
+	<!--
 	<div class="container-fluid white">
 		<h2>Activities <br /><i>Main Event Hall</i></h2>
 	</div>
@@ -66,7 +70,7 @@ Template Name: Schedule Template
 		<div class="container-fluid">
 			<div class="col-sm-3">
 				<h2>11:15AM</h2>
-			</div>	
+			</div>
 			<div class="col-sm-9">
 				<h2>Flowforms Yoga</h2>
 				<p>Enjoy a family-friendly yoga session guided by local teachers from Worcester's Flowforms Yoga. Yoga makes your mind calm and peaceful and opens your heart. It tones, strengthens and heals your body. Presentation is appropriate for all ages, fitness levels and body types. </p>
@@ -78,7 +82,7 @@ Template Name: Schedule Template
 		<div class="container-fluid">
 			<div class="col-sm-3">
 				<h2>12:15PM</h2>
-			</div>	
+			</div>
 			<div class="col-sm-9">
 				<h2>Children's Book Reading</h2>
 				<p>Lena of Vegitopia and the Mystery of the Missing Animals is a vegan-themed fairy tale about how one brave little girl stands up for the animal friends of her land and helps rescue them from being eaten. The book promotes messages of kindness, compassion, and action and shows that magical things can happen when you harness the power of veggies.</p>
@@ -90,22 +94,24 @@ Template Name: Schedule Template
 		<div class="container-fluid">
 			<div class="col-sm-3">
 				<h2>1:15PM</h2>
-			</div>	
+			</div>
 			<div class="col-sm-9">
 				<h2>Vegetable Circus</h2>
 				<p>Vegetable Circus instructors teach the basics of a variety of circus arts to deliver an interactive experience. Everyone participates in circus themed group games and receives hands-on instruction in many circus skills. Fun for kids!</p>
 				<p><a href="http://vegetablecircus.com" target="_blank">Vegetable Circus</a></p>
 			</div>
 		</div>
+-->
 	</div>
+<!--
 	<div class="presentation yellow">
 		<div class="container-fluid">
 			<div class="col-sm-3">
 				<h2>2:15PM</h2>
-			</div>	
+			</div>
 			<div class="col-sm-9">
 				<h2>Children's Book Reading</h2>
-				<p>Vegan Publishers do a live reading of <i>Dave Loves Chickens</i>. Do you love chickens? Dave does! Dave is a lovely monster from far far away that doesn’t understand why humans eat animals. Come with Dave and explore the fascinating world of chickens and their many talents. Written and illustrated by Carlos Patino. 
+				<p>Vegan Publishers do a live reading of <i>Dave Loves Chickens</i>. Do you love chickens? Dave does! Dave is a lovely monster from far far away that doesn’t understand why humans eat animals. Come with Dave and explore the fascinating world of chickens and their many talents. Written and illustrated by Carlos Patino.
 				<p><a href="http://veganpublishers.com/multimedia-archive/dave-loves-chickens/" target="_blank">Vegan Publishers: Dave Loves Chickens</a></p>
 			</div>
 		</div>
@@ -114,7 +120,7 @@ Template Name: Schedule Template
 		<div class="container-fluid">
 			<div class="col-sm-3">
 				<h2>3:15PM</h2>
-			</div>	
+			</div>
 			<div class="col-sm-9">
 				<h2>Vegetable Circus</h2>
 			</div>
@@ -124,11 +130,11 @@ Template Name: Schedule Template
 		<div class="container-fluid">
 			<div class="col-sm-3">
 				<h2>4:15PM</h2>
-			</div>	
+			</div>
 			<div class="col-sm-9">
 				<h2>Flowforms Yoga</h2>
 			</div>
 		</div>
-	</div>
+	</div> -->
 
 </div><!--/content-->
