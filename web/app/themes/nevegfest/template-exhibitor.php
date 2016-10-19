61e7b5cf413c6ee6e7a591c424ebf588ac97804e
newenglandvegfest <newenglandvegfest@harrisburg.dreamhost.com>
2016-10-19 12:21:52 -0700

    i dont know what these changes represent, I think @gopperman did this. commiting because seems like it should be. -Drew

--- web/app/themes/nevegfest/template-exhibitor.php
+++ web/app/themes/nevegfest/template-exhibitor.php
@@ -6,7 +6,7 @@ Template Name: Exhibitor Template
 <?php get_template_part('templates/page', 'header'); ?>
 <div class="content">
 <div class="container-fluid" id="about" class="32">
-  <p>The New England VegFest is a free festival that brings the local community together to celebrate vegetarianism: an animal-friendly, environmentally sustainable, and healthy lifestyle. The VegFest began in 2010 as the Worcester VegFest. This year, we're growing to encompass all of New England.</p>
+  <p>The New England VegFest is a free festival that brings the local community together to celebrate vegetarianism: an animal-friendly, environmentally sustainable, and healthy lifestyle. The VegFest began in 2010 as the Worcester VegFest. We've grown to encompass all of New England, reaching thousands and thousands of people in the process.</p>
 </div>
 <div id="features">
 	<div class="box"><p><b>Free</b><br />Admission</p></div>
@@ -20,7 +20,7 @@ Template Name: Exhibitor Template
 		<div class="col-xs-6 col-sm-3"><div class="circle"><b>3,000</b><br />attendees in 2012</div></div>
 		<div class="col-xs-6 col-sm-3"><div class="circle"><b>5,000</b><br />attendees in 2013</div></div>
 		<div class="col-xs-6 col-sm-3"><div class="circle"><b>7,000</b><br />attendees in 2014</div></div>
-		<div class="col-xs-6 col-sm-3"><div class="circle"><b>10,000</b><br />attendees in 2015</div></div>
+		<div class="col-xs-6 col-sm-3"><div class="circle"><b>10,000</b><br />attendees in 2015-16</div></div>
 	</div>
 </div>
 <div id="reasons">
@@ -50,7 +50,7 @@ Template Name: Exhibitor Template
 </div>
 <div id="testimonial" class="32">
   <div class="container-fluid">
-    <p class="alignleft">As someone who has spoken at-and attended-many vegetarian festivals throughout the United States, I've come to see the Worcester VegFest as one of the best in the country...the fest is always so well-attended, well-organized, and it teems with high-quality vendors and speakers."</p>
+    <p class="alignleft">As someone who has spoken at-and attended-many vegetarian festivals throughout the United States, I've come to see the New England VegFest as one of the best in the country...the fest is always so well-attended, well-organized, and it teems with high-quality vendors and speakers."</p>
     <p class="alignright">-Jon Camp, The Humane League</p>
   </div>
 </div>
@@ -61,43 +61,42 @@ Template Name: Exhibitor Template
   <ul>
     <li><img src="/app/themes/nevegfest/assets/img/crowd2.jpg" alt="" />
     <li><img src="/app/themes/nevegfest/assets/img/chips.jpg" alt="" />
-    <li><img src="/app/themes/nevegfest/assets/img/hummus.jpg" alt="" />
-    <li><img src="/app/themes/nevegfest/assets/img/merch.jpg" alt="" />
+    <li><img src="/app/themes/nevegfest/assets/img/programs.jpg" alt="" />
     <li><img src="/app/themes/nevegfest/assets/img/cupcakes2.jpg" alt="" />
+    <li><img src="/app/themes/nevegfest/assets/img/dcucrowd.jpg" alt="" />
   </ul>
 </div>
 <div class="container-fluid white" id="sponsor-info">
 	<h2>Sponsorship Levels</h2>
 <div id="field_1_19" class="gfield gfield_html gfield_html_formatted gfield_no_follows_desc" style=""><div class="col-sm-4">
-<h3>Gold</h3>
+<h3>Corporate</h3>
 <ul>
-<li> All Bronze and Silver benefits, plus:
+<li> All Small Business benefits, plus:
 </li><li>Inside or back cover ad on event program
 </li><li>Social media promotion campaign via Facebook and Twitter
 </li><li>Logo on all VegFest promotional materials (Posters, flyers, print ads, etc.)
-</li><li>VegWorcester will distribute branded materials along with program.
+</li><li>Premium Space with added features (extra space, pipe and drape, line management, etc)
+</li><li>Personalized promotion opportunities during event
 </li></ul>
 </div>
 <div class="col-sm-4">
-<h3>Silver</h3>
+<h3>Small Business</h3>
 <ul>
-<li> All Bronze benefits, plus:
+<li> All Friends and Family benefits, plus:
 </li><li>Special Mention during event
 </li><li>Social Media thank you via Facebook and Twitter
 </li><li>Logo in sponsor section of event program
-</li><li>Free table at VegFest w/ Extras (+ Volunteer staffing)
 </li><li>Premium table placement at event
-</li><li>Drop-ship merchandise to venue
-</li><li>1/2 page ad in program (first come, first serve)
+</li><li>Personalized sponsor benefits
 
 </li></ul>
 </div>
 <div class="col-sm-4">
-<h3>Bronze</h3>
+<h3>Friends and Family</h3>
 <ul>
 <li>Thank you in program and on Website
 </li><li>Free table at VegFest with Electricity and Tables/Chairs
-</li><li>Premium table placement at event
+</li><li>Preferred table placement at event
 </li></ul>
 </div></div>
 	<a href="/sponsor-and-exhibitor-application/" class="cta yellow">Apply Now</a>
@@ -105,11 +104,11 @@ Template Name: Exhibitor Template
 </div>
 <div id="exhibitor-info">
 	<div class="container-fluid">
-<li id="field_1_20" class="gfield      gfield_html gfield_html_formatted gfield_no_follows_desc" style="display: table;"><div class="col-sm-4">
+<li id="field_1_20" class="gfield      gfield_html gfield_html_formatted gfield_no_follows_desc" style="display: table;"><div class="col-sm-4 col-sm-6">
 <h3>Standard Exhibitor Space</h3>
 <b>10'x10' exhibit space for any business, restaurant, or organization.</b>
 </div>
-<div class="col-sm-4">
+<div class="col-sm-4 col-sm-6">
 <h3>Exhibitor Space with Electricity</h3>
 <b>10'x10' exhibit space for any business, restaurant, or organization that requires access to electricity.</b>
 </div>
