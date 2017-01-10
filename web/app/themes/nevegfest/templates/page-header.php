e0a367acab764fbebd8cb4f8a58c19836453311a
Thanks No Thanks <drew.r.wilson@gmail.com>
2017-01-10 16:27:58 -0500

    add interested button

--- web/app/themes/nevegfest/templates/page-header.php
+++ web/app/themes/nevegfest/templates/page-header.php
@@ -4,7 +4,8 @@
   </h1>
 		<?php if (is_front_page()) { ?>
 			<div class="ctas">
-        <a class="cta" href="//www.facebook.com/NewEnglandVegFest/" target="_blank"><i class="fa fa-facebook-square"></i> Like us on Facebook</a>
+        <a class="cta" href="/exhibiting">I'm interested in being a vendor!</a>
+        <a class="cta yellow" href="//www.facebook.com/NewEnglandVegFest/" target="_blank"><i class="fa fa-facebook-square"></i> Like us on Facebook</a>
         <!-- <a class="cta yellow" href="/member/">Become a member - $25</a> -->
 			</div>
 			<h2>Sunday April 30, 2017 &middot; 11AM-5PM &middot; DCU Center &middot; Worcester, MA</h2>
