6a84b9fa5b298f4a34e8402c0a8a56e57ff8faaf
Thanks No Thanks <drew.r.wilson@gmail.com>
2017-02-24 16:37:38 -0500

    exhibitor options, oops

--- web/app/themes/nevegfest/template-exhibitor.php
+++ web/app/themes/nevegfest/template-exhibitor.php
@@ -114,15 +114,15 @@ Template Name: Exhibitor Template
 </div>
 <div id="exhibitor-info">
 	<div class="container-fluid">
-<li id="field_1_20" class="gfield      gfield_html gfield_html_formatted gfield_no_follows_desc" style="display: table;"><div class="col-sm-4 col-sm-6">
-<div class="col-xs-6 col-sm-6">
-  <h3>Standard Exhibitor Space</h3>
-  <b>A single booth space for any business, restaurant, or organization.</b>
-</div>
-<div class="col-xs-6 col-sm-6">
-  <h3>Food Truck Space</h3>
-  <b>A single extended booth space to fit a food truck.</b>
-</div></li>
+    <li id="field_1_20" class="gfield      gfield_html gfield_html_formatted gfield_no_follows_desc" style="display: table;">
+    <div class="col-xs-6 col-sm-6">
+      <h3>Standard Exhibitor Space</h3>
+      <b>A single booth space for any business, restaurant, or organization.</b>
+    </div>
+    <div class="col-xs-6 col-sm-6">
+      <h3>Food Truck Space</h3>
+      <b>A single extended booth space to fit a food truck.</b>
+    </div></li>
 	</div>
 	<a href="/exhibit/?src=sponsorandexhibitorguide" class="cta">Apply to Exhibit</a>
 </div>
