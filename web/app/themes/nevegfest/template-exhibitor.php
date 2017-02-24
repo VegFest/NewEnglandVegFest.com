4c5c84a4d94e0d4223090573aa229f913040e3fe
Thanks No Thanks <drew.r.wilson@gmail.com>
2017-02-24 16:48:42 -0500

    paddingsz

--- web/app/themes/nevegfest/template-exhibitor.php
+++ web/app/themes/nevegfest/template-exhibitor.php
@@ -120,7 +120,7 @@ Template Name: Exhibitor Template
         <b>A single booth space for any business, restaurant, or organization.</b>
       </div>
 
-      <div class="col-sm-offset-2 col-sm-4">
+      <div class="col-sm-4">
         <h3>Food Truck Space</h3>
         <b>A single extended booth space to fit a food truck.</b>
       </div>
