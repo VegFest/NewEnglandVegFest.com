ab15b88929677a3d3ce046fff1b3983cce1ba41a
Thanks No Thanks <drew.r.wilson@gmail.com>
2017-04-04 14:48:38 -0400

    remove price from button

--- web/app/themes/nevegfest/templates/page-header.php
+++ web/app/themes/nevegfest/templates/page-header.php
@@ -6,7 +6,7 @@
 			<div class="ctas">
         <!-- <a class="cta" href="/exhibit/?src=frontpage">Apply to Exhibit!</a> -->
         <a class="cta" href="//www.facebook.com/events/765063066992237/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP ON FACEBOOK</a>
-        <a class="cta yellow" href="/member/?src=frontpage">Become a member - $35</a>
+        <a class="cta yellow" href="/member/?src=frontpage">Become a member</a>
 			</div>
 			<h2>Sunday April 30, 2017 &middot; 11AM-5PM &middot; DCU Center &middot; Worcester, MA</h2>
 		<?php } else { ?>
