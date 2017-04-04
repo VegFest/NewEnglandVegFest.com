95c7e43396944ac9263e3033761184ce788b3769
Thanks No Thanks <drew.r.wilson@gmail.com>
2017-04-04 11:09:44 -0400

    change buttons

--- web/app/themes/nevegfest/templates/page-header.php
+++ web/app/themes/nevegfest/templates/page-header.php
@@ -4,9 +4,9 @@
   </h1>
 		<?php if (is_front_page()) { ?>
 			<div class="ctas">
-        <a class="cta" href="/exhibit/?src=frontpage">Apply to Exhibit!</a>
-        <a class="cta yellow" href="//www.facebook.com/events/765063066992237/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP ON FACEBOOK</a>
-        <!-- <a class="cta yellow" href="/member/">Become a member - $35</a> -->
+        <!-- <a class="cta" href="/exhibit/?src=frontpage">Apply to Exhibit!</a> -->
+        <a class="cta" href="//www.facebook.com/events/765063066992237/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP ON FACEBOOK</a>
+        <a class="cta yellow" href="/member/?src=frontpage">Become a member - $35</a>
 			</div>
 			<h2>Sunday April 30, 2017 &middot; 11AM-5PM &middot; DCU Center &middot; Worcester, MA</h2>
 		<?php } else { ?>
