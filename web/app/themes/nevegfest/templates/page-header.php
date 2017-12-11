8020b3cfb29dbed331b5af2877a11a52acd3ddf4
Thanks No Thanks <drew.r.wilson@gmail.com>
2017-12-11 15:52:29 -0500

    add member signup, add date

--- web/app/themes/nevegfest/templates/page-header.php
+++ web/app/themes/nevegfest/templates/page-header.php
@@ -6,11 +6,10 @@
 			<div class="ctas">
         <!-- <a class="cta" href="/exhibit/?src=frontpage">Apply to Exhibit!</a> -->
         <!--<a class="cta" href="//www.facebook.com/events/765063066992237/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP ON FACEBOOK</a>-->
-        
+        <a class="cta" href="//member.vegfest.org" target="_blank">BECOME A MEMBER</a>
         <a class="cta yellow" href="//NewEnglandVegFest.com/sponsor-exhibitor-guide/"> Exhibit/Sponsor at VegFest</a>
 			</div>
-			<!-- <h2>Sunday April 30, 2017 &middot; 11AM-5PM &middot; DCU Center &middot; Worcester, MA</h2> -->
-      <h2>Spring 2018 &middot; Worcester, MA</h2>
+			<h2>Sunday April 29, 2018 &middot; DCU Center &middot; Worcester, MA</h2>
 		<?php } else { ?>
 			<h2><?= the_title(); ?></h2>
 		<?php } ?>
