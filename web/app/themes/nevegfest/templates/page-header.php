4f176ee5760ab0081c6cad8afcd43e603c9c22e5
Thanks No Thanks <drew.r.wilson@gmail.com>
2018-02-26 10:41:25 -0500

    buttons2

--- web/app/themes/nevegfest/templates/page-header.php
+++ web/app/themes/nevegfest/templates/page-header.php
@@ -4,15 +4,9 @@
   </h1>
 		<?php if (is_front_page()) { ?>
 			<div class="ctas">
-        <!-- <a class="cta" href="/exhibit/?src=frontpage">Apply to Exhibit!</a> -->
-<<<<<<< HEAD
-        <a class="cta yellow" href="/member/?src=frontpage">Become a member</a>
-        <a class="cta" href="//www.facebook.com/events/765063066992237/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP ON FACEBOOK</a>
-=======
-        <!--<a class="cta" href="//www.facebook.com/events/765063066992237/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP ON FACEBOOK</a>-->
         <a class="cta" href="//member.vegfest.org" target="_blank">BECOME A MEMBER</a>
         <a class="cta yellow" href="//NewEnglandVegFest.com/exhibit/?src=frontpage2018"> Exhibit/Sponsor at VegFest</a>
->>>>>>> 5c970d491bb215cebffda1d0c0344c677a606bb2
+        <a class="cta" href="//www.facebook.com/events/765063066992237/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP ON FACEBOOK</a>
 			</div>
 			<h2>Sunday April 29, 2018 &middot; DCU Center &middot; Worcester, MA</h2>
 		<?php } else { ?>
