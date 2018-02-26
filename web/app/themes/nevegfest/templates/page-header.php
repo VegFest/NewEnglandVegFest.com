b766c9da2dea4263c4d9582d57fc0efbe0c2cc7d
Thanks No Thanks <drew.r.wilson@gmail.com>
2018-02-26 10:43:25 -0500

    fb button

--- web/app/themes/nevegfest/templates/page-header.php
+++ web/app/themes/nevegfest/templates/page-header.php
@@ -4,9 +4,9 @@
   </h1>
 		<?php if (is_front_page()) { ?>
 			<div class="ctas">
-        <a class="cta" href="//member.vegfest.org" target="_blank">BECOME A MEMBER</a>
+        <a class="cta" href="//member.vegfest.org?src=nevf18" target="_blank">BECOME A MEMBER</a>
         <a class="cta yellow" href="//NewEnglandVegFest.com/exhibit/?src=frontpage2018"> Exhibit/Sponsor at VegFest</a>
-        <a class="cta" href="//www.facebook.com/events/765063066992237/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP ON FACEBOOK</a>
+        <a class="cta" href="//facebook.com/events/144050729606900/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP ON FACEBOOK</a>
 			</div>
 			<h2>Sunday April 29, 2018 &middot; DCU Center &middot; Worcester, MA</h2>
 		<?php } else { ?>
