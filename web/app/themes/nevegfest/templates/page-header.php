403d4ab2abe6bed84a070bd1c84de4071a02847b
Drew <drew.r.wilson@gmail.com>
2019-01-30 16:44:34 -0500

    change date

--- web/app/themes/nevegfest/templates/page-header.php
+++ web/app/themes/nevegfest/templates/page-header.php
@@ -8,7 +8,7 @@
         <a class="cta" href="//facebook.com/events/144050729606900/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP ON FACEBOOK</a>
         <a class="cta yellow" href="//member.vegfest.org?src=nevf18" target="_blank">BECOME A MEMBER & GET EARLY ADMISSION</a>
 			</div>
-			<h2>Sunday April 29, 2018 &middot; Noon-5pm &middot; DCU Center &middot; Worcester, MA</h2>
+			<h2>Sunday April 28, 2019 &middot;  DCU Center &middot; Worcester, MA</h2>
 		<?php } else { ?>
 			<h2><?= the_title(); ?></h2>
 		<?php } ?>
