a2c1b3a850d0521327c69e422f8322102c8d276e
Drew <drew.r.wilson@gmail.com>
2019-02-27 15:14:07 -0800

    add buttons for sponsor/exhibit

--- web/app/themes/nevegfest/templates/page-header.php
+++ web/app/themes/nevegfest/templates/page-header.php
@@ -4,7 +4,8 @@
   </h1>
 		<?php if (is_front_page()) { ?>
 			<div class="ctas">
-        <!-- <a class="cta yellow" href="//NewEnglandVegFest.com/exhibit/?src=frontpage2018"> Exhibit/Sponsor at VegFest</a> -->
+        <a class="cta red" href="//NewEnglandVegFest.com/sponsor/?src=frontpage2019"> Sponsor</a>
+        <a class="cta yellow" href="//NewEnglandVegFest.com/exhibit/?src=frontpage2019"> Exhibit</a>
         <!-- <a class="cta" href="//facebook.com/events/144050729606900/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP ON FACEBOOK</a> -->
         <a class="cta yellow" href="//member.vegfest.org?src=nevf19" target="_blank">BECOME A MEMBER & GET EARLY ADMISSION</a>
 			</div>
