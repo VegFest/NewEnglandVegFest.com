61c9bd8f819a31158395b94e0f993786a422dbbc
Drew <drew.r.wilson@gmail.com>
2019-03-29 12:43:47 -0400

    add new sponsors

--- web/app/themes/nevegfest/front-page.php
+++ web/app/themes/nevegfest/front-page.php
@@ -71,11 +71,9 @@
 		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2017/web-logos_17-02-lightlife.png" alt="Lightlife" />
 		<!--  -->
 		<!-- <img src="/app/themes/nevegfest/assets/img/sponsor-logos/2018/navitas.png" alt="Navitas" />
-		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2018/like-no-udder.png" alt="Like No Udder" />
+
 		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2017/web-logos_17-03-thehumaneleague.png" alt="The Humane League" />
-		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2017/web-logos_17-04-awellfedworld.png" alt="A Well-fed World" />
 		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2017/web-logos_17-08-womag.png" alt="Worcester Magazine" />
-		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2017/web-logos_17-07-roots.png" alt="Roots" />
 
 		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2017/web-logos_17-14-loving-hut.png" alt="Loving Hut" />
 		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2018/dcu-center.png" alt="DCU Center" />
@@ -105,6 +103,9 @@
 		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2018/hippeas.png" alt="Hippeas" />
 		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2017/web-logos_17-15-vegan-treats.png" alt="Vegan Treats" />
 		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2018/yoga.png" alt="Yoga at the Ashram" />
+		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2018/like-no-udder.png" alt="Like No Udder" />
+		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2017/web-logos_17-04-awellfedworld.png" alt="A Well-fed World" />
+		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2017/web-logos_17-07-roots.png" alt="Roots" />
 		<!--
 		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2017/web-logos_17-06-organiclivingsuperfoods.png" alt="Organic Living Superfoods" />
 
