0ca91d23b87686c3e4a61362839065d4c03597bf
Drew <drew.r.wilson@gmail.com>
2019-04-15 16:43:59 -0400

    add first draft of schedule

--- web/app/themes/nevegfest/template-schedule.php
+++ web/app/themes/nevegfest/template-schedule.php
@@ -12,16 +12,14 @@ Template Name: Schedule Template
 	<div class="presentation green">
 		<div class="container-fluid">
 			<div class="col-sm-3">
-				<h2>12:30PM-1:15pm</h2>
-				<img src="/app/themes/nevegfest/assets/img/presenters2018/chef-kevin.jpg" alt="Chef Kevin Williams" />
+				<h2>12:30PM-1:20pm</h2>
+				<img src="/app/uploads/gravity_forms/31-039a11cce4b26d01f50a5f40d2559bd5/2019/03/Kathy_Hester_2017-66-1-1.jpg" alt="Kathy	Hester" />
 			</div>
 			<div class="col-sm-9">
-				<h2>Chef Kevin Williams <i>ROOTS Natural Foods. Kitchen + Juice Bar</i></h2>
-				<p>He's back again this year for another cooking demo.</p>
-				<p>Bio: Kevin Williams has been cooking professionally for over 25 years. His culinary path began at the age of 14 when he declared to his mother he was going to stop eating animals. Chef Kevin attended cooking school at Johnson & Wales in Providence, RI, and he has held the chef position in 5 US states as well as in Canada. At Roots, Chef Kevin has brought his California culinary style to the east, utilizing fresh farm produce, organic grains, tubers, and legumes.
-				</p>
-				<h3>Cooking Demo:  Introduction to Vegan Mexican Cooking</h3>
-				<p>During this vegan cooking demo, Chef Kevin Williams from Roots Natural Foods will put his own twist on basic sauce making. On the menu: ceviche and jackfruit tacos using traditional techniques that he recently perfected on a trip through Mexico. </p>
+				<h2>Kathy	Hester <i>Vegan Cookbook Author/Blogger</i></h2>
+				<h3>Instant Pot: Perfect for Vegan Kitchens</h3>
+				<p>Kathy Hester is passionate about making healthy eating easy and delicious. You can find her recipes on HealthySlowCooking.com and PlantBasedInstantPot.com, and on her private Facebook group, Vegan Recipes: Cooking with Kathy Hester. Her recipes are so full of flavor even the meat eaters in your family will love them too. She is the author of the bestselling cookbooks, The Revised Vegan Slow Cooker and The Ultimate Vegan Cookbook for Your Instant Pot.</p>
+
 			</div>
 		</div>
 	</div>
@@ -29,13 +27,14 @@ Template Name: Schedule Template
 		<div class="container-fluid">
 
 			<div class="col-sm-3">
-				<h2>1:30PM-2:30PM</h2>
-				<img src="/app/themes/nevegfest/assets/img/presenters2018/Miyoko-Schinner.jpg" alt="Miyoko Schinner" />
+				<h2>1:45PM-2:05PM</h2>
+				<img src="/app/uploads/gravity_forms/31-039a11cce4b26d01f50a5f40d2559bd5/2019/03/IMG_5079.jpg" alt="Berto	Calkins" />
 			</div>
 			<div class="col-sm-9">
-				<h2>Miyoko Schinner <i>Author & Entrepreneur</i></h2>
-				<h3>Presentation: Is the Future of Food Vegan?</h3>
-				<p>Miyoko Schinner is founder and CEO of Miyoko’s Kitchen, a leader in innovative, award-winning, organic dairy alternatives that aim to redefine the future of the creamery. A passionate culinarian, Miyoko is the author of five cookbooks and hosted three seasons of Vegan Mashup, a cooking show that aired on PBS nationwide. An avid activist working to change the culture of food, Miyoko is on the board of the Plant Based Foods Association and co-founder of Rancho Compasión, a farmed animal sanctuary in Nicasio, California that provides a home to over 80 farm animals.</p>
+				<h2>Berto	Calkins <i>Fitness Professional</i></h2>
+				<h3>Presentation: Optimal on Plants</h3>
+				<p>Berto Calkins is an NASM Certified Personal Trainer and NASM Certified Fitness Nutrition Specialist who has been active in fitness for 14 years. He currently has a workout program out called "Pyramids and also offer custom meal plans to online clients. He also holds a B.A in Communications (Advertising/ Public Relations) from the City College of New York.</p>
+				<p>Berto showcases content that promotes mental & physical wellness, as well as comfort foods that encourage non-vegans to become interested in the lifestyle. His mission is to make people aware of controllable influences on their lives, so that they can make decisions that better themselves and the world. Berto also serves on the board of directors at Woodstock Farm Sanctuary.</p>
 			</div>
 
 		</div>
@@ -43,53 +42,31 @@ Template Name: Schedule Template
 	<div class="presentation yellow">
 		<div class="container-fluid">
 			<div class="col-sm-3">
-				<h2>3:30PM-5PM</h2>
-				<img src="/app/themes/nevegfest/assets/img/presenters2018/Honey-LaBronx.jpg" alt="Honey LaBronx" />
+				<h2>3:15PM-4:05PM</h2>
+				<img src="/app/uploads/gravity_forms/31-039a11cce4b26d01f50a5f40d2559bd5/2019/03/Toni-Okamoto-Farmers-Market-Shoot-7.jpg , http://newenglandvegfest.com/app/uploads/gravity_forms/31-039a11cce4b26d01f50a5f40d2559bd5/2019/03/Toni-Okamoto-Farmers-Market-Shoot-12.jpg" alt="Toni	Okamoto" />
 			</div>
 			<div class="col-sm-9">
-				<h2>Honey LaBronx<i> Activist</i></h2>
-				<p>Honey LaBronx stars in her own YouTube cooking series, The Vegan Drag Queen, and hosts the Big Fat Vegan Radio podcast. An LGBTQ rights activist, she is committed to bringing discussions about animal rights, intersectionality, and social justice to new audiences. In 2011, she was one of 8 activists arrested for blocking New York City traffic while demonstrating for marriage equality alongside her drag mother, Bob The Drag Queen. She has performed around the world raising funds for local animal sanctuaries, rescues, and activist organizations. Out of drag Honey — Ben Strothmann — is an actor, singer, playwright, and theatrical photographer, originally from Milwaukee, Wisconsin.</p>
-				<h3>Presentation: Listening Powerfully</h3>
-				<p> Have your conversations left you feeling disempowered? Does it seem like there's a bigger impact to be had? Are you sometimes unsuccessful in getting through to people?
-
-Come discover some of the fundamental rules of communication. This workshop will feature one-on-one coaching and partnered sharing exercises in a relaxed and fun setting. Participants will leave having discovered for themselves that it's not so much about HOW to listen — but what specifically to listen FOR in order to have conversations that make things happen.</p>
+				<h2>Toni Okamoto<i> Author of Plant-Based on a Budget</i></h2>
+				<h3>Eating Plant-Based on a Budget</h3>
+				<p>Toni Okamoto is the founder of Plant-Based on a Budget, the popular website and meal plan that shows you how to save dough by eating veggies. She’s also author of the forthcoming book, Plant-Based on a Budget, The Super Easy Vegan Slow Cooker Cookbook, and the co-host of The Plant-Powered People Podcast. She's a regular presence on the FOX affiliate in Sacramento, where she teaches viewers how to break their meat habit without breaking their budget, and was featured in the popular documentary What the Health.</p>
 			</div>
 		</div>
 	</div>
-
-	<!-- <div class="presentation red">
-		<div class="container-fluid">
-			<div class="col-sm-3">
-				<h2>3:30PM</h2>
-				<img src="/app/themes/nevegfest/assets/img/speakers/lorelei-plotczyk.jpg" alt="Lorelei Plotczyk" />
-			</div>
-			<div class="col-sm-9">
-				<h2>Lorelei Plotczyk, <i>Truth or Drought</i></h2>
-
-				<p>
-					Lorelei Plotczyk is an environmentalist and writer. She holds an MBA with a specialization in Environmental Management and is organizer of <a href="http://www.truthordrought.com/">Truth or Drought</a>, a campaign that draws connections between animal agriculture, water scarcity and other environmental issues. Come to this presentation for a fun and upbeat way to learn about the widespread ecological impacts of animal farming. Lorelei blogs at <a href="http://eatplantsdrinkbeer.com/">Eat Plants / Drink Beer</a> and <a href="https://www.facebook.com/brainonhugs">Brain on Hugs</a>.
-				</p>
-
-
-			</div>
-		</div>
-	</div> -->
-
 
 
 	<div class="container-fluid white">
-		<h2>Activities <br /><i>Meeting Room E, 3rd Floor</i></h2>
+		<h2>Presentations <br /><i>Meeting Room E, 3rd Floor</i></h2>
 	</div>
 	<div class="presentation green">
 		<div class="container-fluid">
 			<div class="col-sm-3">
-				<h2>12:15AM-1PM</h2>
-				<img src="/app/themes/nevegfest/assets/img/yoga-at-the-ashram.png" alt="Yoga at the Ashram" />
+				<h2>1:15AM-2:05PM</h2>
+				<img src="/app/uploads/gravity_forms/31-039a11cce4b26d01f50a5f40d2559bd5/2019/03/Maya-author-2.jpeg" alt="Maya Gottfried" />
 			</div>
 			<div class="col-sm-9">
-				<h2>Morning Yoga</h2>
-				<h3>Provided by Yoga at the Ashram</h3>
-				<p>Start the day off right and join Yoga at the Ashram for some free family-friendly yoga session. Explore yoga postures and guided visualizations to create an engaging and relaxing experience for the whole family. </p>
+				<h2>Maya Gottfried <i>Author</i></h2>
+				<h3>Navigating Your Love Life as a Vegan/Vegetarian</h3>
+				<p>Maya Gottfried is the author of Vegan Love: Dating and Partnering for the Cruelty-Free Gal (Skyhorse), and books for children including Our Farm: By the Animals of Farm Sanctuary (Knopf). Her articles have been published in outlets including VegNews, KrisCarr.com, ForksOverKnives.com, Lilith Magazine, and People online.</p>
 
 			</div>
 		</div>
@@ -97,70 +74,117 @@ Come discover some of the fundamental rules of communication. This workshop will
 	<div class="presentation red">
 		<div class="container-fluid">
 			<div class="col-sm-3">
-				<h2>1:30PM-3:30PM</h2>
-				<!-- <img src="/app/themes/nevegfest/assets/img/presenters2018/" alt="Lena of Vegitopia" /> -->
+				<h2>2:15PM-3:05PM</h2>
+				<img src="/app/uploads/gravity_forms/31-039a11cce4b26d01f50a5f40d2559bd5/2019/03/Leshko-author-photo-credit-Ron-Cowie.jpg" alt="" />
 			</div>
 			<div class="col-sm-9">
-				<h2>Movie Screening</h2>
-
-				<p>Film to be announced</p>
+				<h2>Isa	Leshko <i>Artist & Author</i></h2>
+				<h3>Allowed to Grow Old: What I Learned Creating Portraits of Animals at Farm Sanctuaries</h3>
+				<p>Isa Leshko is an artist and writer who focuses on themes of animal rights and aging. Her images of elderly farm animals are much admired and have been published in the Atlantic, Boston Globe, the Guardian, Harper’s, the New York Times, and elsewhere. Her first book, Allowed to Grow Old: Portraits of Elderly Rescued Farm Animals, was recently released by the University of Chicago Press. The book includes essays by activist Gene Baur, NY Times bestselling author Sy Montgomery, and curator Anne Wilkes Tucker.</p>
 			</div>
 		</div>
 	</div>
 	<div class="presentation blue">
 		<div class="container-fluid">
 			<div class="col-sm-3">
-				<h2>4PM-4:45PM</h2>
-				<!-- <img src="/app/themes/nevegfest/assets/img/presenters2017/veg-speed-date.png" alt="Veg Speed Dating" /> -->
+				<h2>3:45PM-4:35PM</h2>
+				<img src="/app/uploads/gravity_forms/31-039a11cce4b26d01f50a5f40d2559bd5/2019/03/96770429-792D-4567-98A7-D2ED7403B75A.jpeg" alt="Nicole	Broushet" />
 
 			</div>
 			<div class="col-sm-9">
-				<h2>End of the Day Yoga & Meditation</h2>
-				<p>End your VegFest by joining Yoga at the Ashram for yoga and meditation.</p>
+				<h2>Nicole	Broushet <i>Owner The Vegan Nest</i></h2>
+				<p>"Victor and Nicole Broushet: In 2014, husband and wife team, Victor and Nicole Broushet, founded The Vegan Nest. “The Nest,” as it is affectionately called, is a culinary-nutrition-education and health advocacy organization committed to educating, empowering, and strengthening communities. In 2017, they established The Vegan Nest Café, an organic, farm-to-table, plant-based café based in downtown Worcester.</p>
+ 
+
 			</div>
 		</div>
 
 	</div>
-<!--
-	<div class="presentation yellow">
-		<div class="container-fluid">
-			<div class="col-sm-3">
-				<h2>2:15PM</h2>
-			</div>
-			<div class="col-sm-9">
-				<h2>Children's Book Reading</h2>
-				<p>Vegan Publishers do a live reading of <i>Dave Loves Chickens</i>. Do you love chickens? Dave does! Dave is a lovely monster from far far away that doesn’t understand why humans eat animals. Come with Dave and explore the fascinating world of chickens and their many talents. Written and illustrated by Carlos Patino.
-				<p><a href="http://veganpublishers.com/multimedia-archive/dave-loves-chickens/" target="_blank">Vegan Publishers: Dave Loves Chickens</a></p>
-			</div>
-		</div>
+	<div class="container-fluid white">
+		<h2>Performances & Activities <br /><i>Showcase Corner (main floor)</i></h2>
 	</div>
 	<div class="presentation green">
 		<div class="container-fluid">
 			<div class="col-sm-3">
-				<h2>3:15PM</h2>
+				<h2>11:30AM-12:00PM</h2>
+				<img src="/app/uploads/gravity_forms/31-039a11cce4b26d01f50a5f40d2559bd5/2019/03/2B4CB768-8B37-45D3-869D-AA17C63D5493.jpeg" alt="Grey" />
 			</div>
 			<div class="col-sm-9">
-				<h2>Vegetable Circus</h2>
+				<h2>Grey <i>Musician</i></h2>
+				<h3>Plant Based Drippin</h3>
+				<p>Grey is one of the hottest emerging artists at the intersection between activism, urban style & hip hop. A growing Atlanta institution in his own right, Grey became one of the most talked about acts of 2016 after his Vegan Thanksgiving freestyle & music video took the internet by storm. Notching a total of 30+ million views, he was catapulted from local stardom to the global stage. Even PETA and Whole Foods supported his release, proving the cross-marketability of Grey and his message.</p>
+				<p>His music and clothing line, Plant Based Drippin, is creating a new energy in the Vegan community. A lyricist, activist, and social entrepreneur, Grey is nothing short of refreshing. With a clear, understandable, and sophisticated flow, Grey is an artist to watch in 2019. He, like vegan cooking, just might surprise you</p>
+
 			</div>
 		</div>
 	</div>
 	<div class="presentation red">
 		<div class="container-fluid">
 			<div class="col-sm-3">
-				<h2>4:15PM</h2>
+				<h2>12:45PM-1:30PM</h2>
+				<img src="/app/themes/nevegfest/assets/img/presenters2018/" alt="Yoga at the Ashram" />
 			</div>
 			<div class="col-sm-9">
-				<h2>Flowforms Yoga</h2>
+				<h2>Yoga</h2>
+				<h3>Hosted by Yoga at the Ashram</h3>
+				<p>Yoga at the Ashram is a non-profit organization founded in Millis, MA in 1970.</p>
 			</div>
 		</div>
-	</div> -->
+	</div>
+	<div class="presentation blue">
+		<div class="container-fluid">
+			<div class="col-sm-3">
+				<h2>1:45PM-2:30PM</h2>
+				<img src="/app/themes/nevegfest/assets/img/presenters2018/" alt="Yoga at the Ashram" />
+
+			</div>
+			<div class="col-sm-9">
+				<h2>Kids' Yoga</h2>
+				<h3>Hosted by Yoga at the Ashram</h3>
+				<p>It is named after the great yogi, Baba Siri Chand, who united all of yoga 500 years ago.</p>
+
+			</div>
+		</div>
+
+	</div>
+	<div class="presentation yellow">
+		<div class="container-fluid">
+			<div class="col-sm-3">
+				<h2>3:15PM-4:05PM</h2>
+				<img src="/app/uploads/gravity_forms/31-039a11cce4b26d01f50a5f40d2559bd5/2019/03/2B4CB768-8B37-45D3-869D-AA17C63D5493.jpeg" alt="Grey" />
+			</div>
+			<div class="col-sm-9">
+				<h2>Grey <i>Musician</i></h2>
+				<h3>Plant Based Drippin - 2nd Performance</h3>
+				<p>Grey is one of the hottest emerging artists at the intersection between activism, urban style & hip hop. A growing Atlanta institution in his own right, Grey became one of the most talked about acts of 2016 after his Vegan Thanksgiving freestyle & music video took the internet by storm. Notching a total of 30+ million views, he was catapulted from local stardom to the global stage. Even PETA and Whole Foods supported his release, proving the cross-marketability of Grey and his message.</p>
+				<p>His music and clothing line, Plant Based Drippin, is creating a new energy in the Vegan community. A lyricist, activist, and social entrepreneur, Grey is nothing short of refreshing. With a clear, understandable, and sophisticated flow, Grey is an artist to watch in 2019. He, like vegan cooking, just might surprise you</p>
+			</div>
+		</div>
+	</div>
+	<div class="presentation green">
+		<div class="container-fluid">
+			<div class="col-sm-3">
+				<h2>4PM-4:45PM</h2>
+				<img src="/app/themes/nevegfest/assets/img/presenters2018/" alt="Yoga at the Ashram" />
+
+			</div>
+			<div class="col-sm-9">
+				<h2>Yoga</h2>
+				<h3>Hosted by Yoga at the Ashram</h3>
+				<p>Whether you seek sound healing, Kundalini yoga for awareness, meditation, dance, or a yoga get-away, Yoga at the Ashram is for you. It also offers innovative educational practices & programs, including world-class trainings, supported with Ayurvedic-inspired vegetarian nutrition. It is located in the spiritually uplifting environment of the yoga center’s host, Guru Ram Das Ashram, a place of peaceful meditation for over 45 years.</p>
+
+			</div>
+		</div>
+
+	</div>
+
 	<div class="container-fluid white">
 		<h2>Kids' Stuff! <br /><i>Kids area in the exhibit hall</i></h2>
 	</div>
 	<div class="presentation green">
 		<div class="container-fluid">
 			<div class="col-sm-3">
-				<h2>1PM-1:30PM</h2>
+				<h2>12:15PM-12:45PM</h2>
 				<!-- <img src="/app/themes/nevegfest/assets/img/presenters2018/" alt="Yoga at the Ashram" /> -->
 			</div>
 			<div class="col-sm-9">
@@ -178,9 +202,9 @@ Come discover some of the fundamental rules of communication. This workshop will
 				<!-- <img src="/app/themes/nevegfest/assets/img/presenters2018/" alt="Lena of Vegitopia" /> -->
 			</div>
 			<div class="col-sm-9">
-				<h2>Kids' Yoga</h2>
-
-				<p></p>
+				<h2>Casey	Taft <i>Co-founder Vegan Publishers</i></h2>
+				<h3>Kids' Storytime</h3>
+				<p>Vegan Publishers was formed in 2013 with a mission to publish a range of books that promote social consciousness and awareness related to veganism. We are interested in publishing books for adults and children that focus on positive themes emphasizing justice and respect for all living beings. Vegan Publishers hopes to be at the forefront of the growing societal trend towards vegan living.</p>
 			</div>
 		</div>
 	</div>
