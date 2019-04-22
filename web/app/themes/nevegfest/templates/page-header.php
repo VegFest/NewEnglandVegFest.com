224627a25c7054cb3effe389d243b5a1fba78eb8
Drew <drew.r.wilson@gmail.com>
2019-04-22 14:51:48 -0400

    buttons

--- web/app/themes/nevegfest/templates/page-header.php
+++ web/app/themes/nevegfest/templates/page-header.php
@@ -4,10 +4,10 @@
   </h1>
 		<?php if (is_front_page()) { ?>
 			<div class="ctas">
-        <a class="cta red" href="//NewEnglandVegFest.com/sponsor/?src=frontpage2019"> Sponsor</a>
-        <a class="cta yellow" href="//NewEnglandVegFest.com/exhibit/?src=frontpage2019"> Exhibit</a>
-        <!-- <a class="cta" href="//facebook.com/events/144050729606900/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP ON FACEBOOK</a> -->
-        <a class="cta blue" href="//member.vegfest.org?src=nevf19" target="_blank">GET EARLY ADMISSION</a>
+        <!-- <a class="cta red" href="//NewEnglandVegFest.com/sponsor/?src=frontpage2019"> Sponsor</a> -->
+        <!-- <a class="cta yellow" href="//NewEnglandVegFest.com/exhibit/?src=frontpage2019"> Exhibit</a>         -->
+        <a class="cta" href="//member.vegfest.org?src=nevf19" target="_blank">BECOME A MEMBER & GET EARLY ADMISSION</a>
+        <a class="cta blue" href="//facebook.com/events/2156801437748462/" target="_blank"><i class="fa fa-facebook-square"></i> RSVP ON FACEBOOK</a> -->
 			</div>
 			<h2>Sunday April 28, 2019 12pm-5pm &middot; DCU Center &middot; Worcester, MA</h2>
 		<?php } else { ?>
