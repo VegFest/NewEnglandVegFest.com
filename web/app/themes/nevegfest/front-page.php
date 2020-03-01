1112e9f2ab203d1ae8569e10d9d3144082fa695e
Drew <drew.r.wilson@gmail.com>
2020-02-29 21:28:49 -0500

    add vegan robs

--- web/app/themes/nevegfest/front-page.php
+++ web/app/themes/nevegfest/front-page.php
@@ -101,6 +101,7 @@
 		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2019/spirit-of-change.png" alt="Spirit of Change" />
 		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2020/green-mountain-energy-logo.png" alt="Green Mountain Energy" />
 		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2020/edible-worcester-logo.png" alt="Edible Worcester" />
+		<img src="/app/themes/nevegfest/assets/img/sponsor-logos/2020/vegan-robs-logo.png" alt="Vegan Rob's" />
 		<!-- <img src="/app/themes/nevegfest/assets/img/sponsor-logos/2018/hippeas.png" alt="Hippeas" /> -->
 		<!-- <img src="/app/themes/nevegfest/assets/img/sponsor-logos/2017/web-logos_17-15-vegan-treats.png" alt="Vegan Treats" /> -->
 		<!-- <img src="/app/themes/nevegfest/assets/img/sponsor-logos/2018/yoga.png" alt="Yoga at the Ashram" /> -->
